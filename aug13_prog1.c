4422109be08bee1b76aa617d1f0481bf98ab2750
Vidhya <vidhyaaatripathiii@gmail.com>
2026-08-16 02:49:52 +0530

    100 days of code

---
+++ aug13_prog1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{ int a=10, b=3;
+  printf("Sum=%d\n", a+b);
+  printf("Diff=%d\n", a-b);
+  printf("Product=%d\n", a*b);
+  printf("Quotient=%d\n", a/b);
+  printf("Remainder=%d\n", a%b);
+  return 0;
+}
---
+++ aug13_prog2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{ 
+    int a=5, b=8;
+  printf("%d\n", a==b); //0
+  printf("%d\n", a<b); //1
+  printf("%d\n", a!=b); //1
+  return 0;
+}
