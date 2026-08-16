6018324327954d64775322b5ab15f380cc945493
Vidhya <vidhyaaatripathiii@gmail.com>
2026-08-16 13:58:52 +0530

    100 days of code

---
+++ DAY6_p2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // First check if the number is 0 or not
+    if (num >= 0)
+    {
+        // Nested if-else
+        if (num == 0)
+        {
+            printf("The number is zero.");
+        }
+        else
+        {
+            printf("The number is positive.");
+        }
+    }
+    else
+    {
+        printf("The number is negative.");
+    }
+
+    return 0;
+}
